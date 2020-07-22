1b5589fa7ec5e149e4f8d38d1c7f797eccfc333c
UlisesArroyo <66101708+UlisesArroyo@users.noreply.github.com>
2020-07-21 19:21:28 -0500

    programa_5b

--- programa_5/5_b.py
+++ programa_5/5_b.py
@@ -1,22 +1,41 @@
+"""
 import pdb 
 import torch
 from PIL import Image
 import os
 from torchvision import transforms
 import torchvision.transforms.functional as TF
+import torch.utils.data as data
+"""
+#pdb.set_trace()
 
+import os
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.utils.data as data
+import torchvision
+from torchvision import transforms
 
-pdb.set_trace()
+transform = transforms.Compose([
+    transforms.ToTensor()])
 
 lista = os.listdir(".")
-x = []
+dir = "./1CM1_1_R_#217/"
+train_data = datasets.ImageFolder(root=dir, transform=transform)
 
 for i in range (len(lista)):
     m = Image.open(lista[i])
-    m = TF.to_tensor(m)
-    x.append(m)
+    m = transform(m)
+    m.cuda
+    m = m[None, ::]
+    if i == 0 :
+        t_batch = m
+        t_batch.cuda
+    else :
+        t_batch = torch.cat((t_batch, m), dim = 0)
 
-x = torch.stack(x)
-x.cuda
-print(x.shape)
+print(t_batch.shape)
 
